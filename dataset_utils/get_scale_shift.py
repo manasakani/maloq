fixed omol scaling (custom atomic numbers were needed)

dataset_utils/get_scale_shift.py
@@ -99,6 +99,10 @@ def get_scale_shift(database, dataset_name, rcut=5.0, dtype=torch.float32, reduc
             orbital_starts = graph_targets.orbital_starts
             basis_transformation = graph_targets.basis_transformation
 
+            # Shift back all the diffuse orbitals (which were incremented by 10 in utils_tensor_decomp.py)
+            for atom, orbitals in orbital_basis.items():
+                orbital_basis[atom] = [orb % 10 for orb in orbitals]
+
             node_labels = graph_targets.node_labels
         
         # 3. Compute the scale and shift for each atomic number
@@ -111,7 +115,7 @@ def get_scale_shift(database, dataset_name, rcut=5.0, dtype=torch.float32, reduc
             element_scalar_values[atomic_number].append(orbital_onsite_scalars)
         
         time_end = time.perf_counter()
-        print(f"Time to scale nodes for molecule {i}: {time_end - time_start} seconds", flush=True)
+        print(f"Time to extract node scalars for molecule {i}: {time_end - time_start} seconds", flush=True)
 
     # print(f"Element scalar values: {element_scalar_values}")
 
@@ -214,9 +218,17 @@ def scale_shift_database(database, start_mol, end_mol, rcut_orbitals, orbital_ba
 
         if scale_nodes:
             print(f"Scaling and shifting the node labels in database[{i}]", flush=True)
+
+            max_node_block = data_obj.node_y.max().item()
+            print(f"Maximum node block element before scaling: {max_node_block:.6f}", flush=True)
+
             scaled_data_obj = copy.deepcopy(data_obj)
-            scaled_data_obj.node_y = data_obj.fock_target_object.scale_shift_node_blocks(data_obj.node_y)
+            scaled_data_obj.node_y = data_obj.fock_target_object.scale_shift_node_blocks(data_obj.node_y,
+                                                                                         data_obj.atomic_numbers)
             data_obj = scaled_data_obj
+
+            max_node_block = data_obj.node_y.max().item()
+            print(f"Maximum node block element after scaling: {max_node_block:.6f}", flush=True)
 
         data_list.append(data_obj)
     

fock_utils/fock_targets.py
@@ -221,7 +221,7 @@ class Fock_Targets:
         self.edge_dist[:, 0] = torch.linalg.norm(self.edge_dist[:, 1:4], dim=-1, keepdim=False)                 # Scalar distances
     
     
-    def scale_shift_node_blocks(self, node_blocks):
+    def scale_shift_node_blocks(self, node_blocks, node_atomic_numbers=None):
         """
         Scale the l=0 values in the targets
         scales - a list of scaling factors for each l=0 irrep component
@@ -231,12 +231,15 @@ class Fock_Targets:
         NOTE: if an element does not have that scalar value, the corresponding mean is 0.0 and std is 1.0
         """
 
+        if node_atomic_numbers is None:
+            node_atomic_numbers = self.atomic_numbers
+
         means = self.scale_shift_data['element_scalar_means']
         stds = self.scale_shift_data['element_scalar_stds']
         scalar_indices = self.scale_shift_data['scalar_irrep_indices']
 
         # Process each node block
-        for i, (node_block, z) in enumerate(zip(node_blocks, self.atomic_numbers)):
+        for i, (node_block, z) in enumerate(zip(node_blocks, node_atomic_numbers)):
             z = int(z.item()) if isinstance(z, torch.Tensor) else int(z)
             mean_vals = means[z]
             std_vals = stds[z]