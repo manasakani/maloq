removed input Ndb from distribute_data function (automatically computed), fixed spin dimension for closed shell calculation

dataset_utils/ASEDataset.py
@@ -156,7 +156,7 @@ class sampleDataset(torch.utils.data.Dataset):
 def sample_collate_fn(batch):
     return Batch.from_data_list(batch)
 
-def distribute_data(base_folder, N_db_files, world_size, rank, N_global_train, N_global_val):
+def distribute_data(base_folder, world_size, rank, N_global_train, N_global_val):
     """
     Calculates the file and index ranges for a single rank to load its portion
     of the global dataset, based on predefined total counts for training and validation.
@@ -166,7 +166,6 @@ def distribute_data(base_folder, N_db_files, world_size, rank, N_global_train, N
 
     Args:
         base_folder (str): The common path prefix for the database files.
-        N_db_files (int): Total number of database files (e.g., 16).
         world_size (int): Total number of training ranks (e.g., 64).
         rank (int): The current rank's index (0 to world_size - 1).
         N_global_train (int): The TOTAL number of molecules to be used for training.
@@ -184,12 +183,17 @@ def distribute_data(base_folder, N_db_files, world_size, rank, N_global_train, N
     current_global_idx = 0
     total_molecules_available = 0
 
-    for i in range(N_db_files):
-        db_file = os.path.join(base_folder, f'omol_electrolytes_unsolvated_job_{i}.db')
+    # extract all the .db file names inside base folder:
+    num_db_files = len([f for f in os.listdir(base_folder) if f.endswith('.db')])
+    db_files = [f for f in os.listdir(base_folder) if f.endswith('.db')]
+
+    for i, db_file in enumerate(db_files):
+        db_file = os.path.join(base_folder, db_file)
 
         try:
             db = ase.db.connect(db_file)
             count = db.count()
+            print("Found ", count, " molecules in ", db_file)
         except Exception as e:
             print(f"Warning: Could not open {db_file}. Skipping. Error: {e}")
             count = 0

dataset_utils/get_scale_shift.py
@@ -83,7 +83,7 @@ def get_scale_shift(database, dataset_name, rcut=5.0, dtype=torch.float32, reduc
             if open_shell:
                 node_labels = [mol.node_y_alpha, mol.node_y_beta]
             else:
-                node_labels = mol.node_y[0]
+                node_labels = mol.node_y
             energies = mol.energies
             # forces = mol.forces
 
@@ -134,11 +134,11 @@ def get_scale_shift(database, dataset_name, rcut=5.0, dtype=torch.float32, reduc
                 element_scalar_values_alpha[atomic_number].append(orbital_onsite_scalars_alpha)
                 element_scalar_values_beta[atomic_number].append(orbital_onsite_scalars_beta)
             else:
-                node_block = node_labels[0][atom_ind] # remove spin dimension
+                node_block = node_labels[atom_ind] # remove spin dimension
                 if atomic_number not in element_scalar_values:
                     element_scalar_values[atomic_number] = []
 
-                orbital_onsite_scalars = node_block[0][scalar_indices]
+                orbital_onsite_scalars = node_block[scalar_indices]
                 element_scalar_values[atomic_number].append(orbital_onsite_scalars)
 
         time_end = time.perf_counter()
@@ -307,8 +307,8 @@ def get_scale_shift(database, dataset_name, rcut=5.0, dtype=torch.float32, reduc
                 "element_scalar_stds": element_scalar_stds,    # dict[int -> list[float]]
                 "scalar_irrep_indices": scalar_indices         # list[int]
             }
-            torch.save(scale_shift_data, "./fock_datasets/"+filename)
-            print("Saved scale_shift_data to ./fock_datasets/"+filename, flush=True)
+            torch.save(scale_shift_data, "./fock_utils/"+filename)
+            print("Saved scale_shift_data to ./fock_utils/"+filename, flush=True)
 
 
 def scale_shift_database(database, start_mol, end_mol, rcut_orbitals, orbital_basis, reduce_edge, scale_shift_data, dataset_name='', scale_nodes=False, open_shell=False, train_or_eval='train'):